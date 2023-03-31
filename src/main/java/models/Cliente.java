9f219ca9027633fef9d2bf1992f1331dceceb017
Elkin Moreno <elkinmoreno18@gmail.com>
2023-03-30 19:56:37 -0500

    Commit class client

---
+++ src/main/java/models/Cliente.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Cliente {
+
+    protected int id;
+    protected String nombre, direccion, telefono;
+
+    public Cliente(int id, String nombre, String direccion, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
